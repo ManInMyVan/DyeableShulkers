5f17f546838190c3f2d9fcfd3d32e78a3f4c16e5
ManInMyVan <113963219+ManInMyVan@users.noreply.github.com>
2025-11-19 18:37:50 -0700

    API: rename getEntity() to getShulker()

--- src/main/java/io/github/maninmyvan/dyeableshulkers/DyeableShulkers.java
+++ src/main/java/io/github/maninmyvan/dyeableshulkers/DyeableShulkers.java
@@ -2,13 +2,14 @@ package io.github.maninmyvan.dyeableshulkers;
 
 import io.github.maninmyvan.dyeableshulkers.event.ShulkerDyeEvent;
 import io.github.maninmyvan.dyeableshulkers.utils.Version;
-import org.bukkit.Bukkit;
-import org.bukkit.DyeColor;
+import org.bukkit.*;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.Shulker;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.material.Dye;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -16,14 +17,6 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Optional;
-
-import static org.bukkit.DyeColor.*;
-import static org.bukkit.GameMode.CREATIVE;
-import static org.bukkit.GameMode.SPECTATOR;
-import static org.bukkit.Sound.ITEM_DYE_USE;
-import static org.bukkit.SoundCategory.PLAYERS;
-import static org.bukkit.entity.EntityType.SHULKER;
-import static org.bukkit.inventory.EquipmentSlot.OFF_HAND;
 
 @SuppressWarnings("OptionalAssignedToNull")
 public class DyeableShulkers extends JavaPlugin implements Listener {
@@ -41,7 +34,7 @@ public class DyeableShulkers extends JavaPlugin implements Listener {
         } catch (ArrayIndexOutOfBoundsException ignored) {}
 
         server = new Version(Integer.decode(split[0]), Integer.decode(split[1]), patch);
-        defaultColor = server.isOlderThan(1, 13, 0) ? PURPLE : null;
+        defaultColor = server.isOlderThan(1, 13, 0) ? DyeColor.PURPLE : null;
     }
 
     @Override
@@ -56,34 +49,31 @@ public class DyeableShulkers extends JavaPlugin implements Listener {
         Bukkit.getPluginManager().registerEvents(this, this);
     }
 
-    public static @Nullable Optional<DyeColor> getColor(ItemStack itemStack, Shulker shulker) {
-        final @Nullable Optional<DyeColor> color = getColor(itemStack);
-        return color != null && color.orElse(defaultColor) != shulker.getColor() ? color : null;
-    }
-
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public static void onInteractEntity(@NotNull PlayerInteractEntityEvent event) {
-        if (event.getHand() == OFF_HAND
-                || event.getRightClicked().getType() != SHULKER
-                || event.getPlayer().getGameMode() == SPECTATOR
-                || !event.getPlayer().hasPermission("dyeableshulkers.dye")
+        final Player player = event.getPlayer();
+
+        if (event.getHand() == EquipmentSlot.OFF_HAND
+                || player.getGameMode() == GameMode.SPECTATOR
+                || !(event.getRightClicked() instanceof Shulker)
+                || !player.hasPermission("dyeableshulkers.dye")
         ) return;
 
         final Shulker shulker = (Shulker) event.getRightClicked();
 
-        ItemStack itemStack = event.getPlayer().getInventory().getItemInMainHand();
+        ItemStack itemStack = player.getInventory().getItemInMainHand();
         Optional<DyeColor> optional = getColor(itemStack);
 
         final boolean offhand = optional == null || optional.orElse(defaultColor) == shulker.getColor();
         if (offhand) {
-            itemStack = event.getPlayer().getInventory().getItemInOffHand();
+            itemStack = player.getInventory().getItemInOffHand();
             optional = getColor(itemStack);
             if (optional == null || optional.orElse(defaultColor) == shulker.getColor()) {
                 return;
             }
         }
 
-        final ShulkerDyeEvent dyeEvent = new ShulkerDyeEvent(shulker, event.getPlayer(), optional.orElse(defaultColor));
+        final ShulkerDyeEvent dyeEvent = new ShulkerDyeEvent(shulker, player, optional.orElse(defaultColor));
         Bukkit.getPluginManager().callEvent(dyeEvent);
 
         if (dyeEvent.isCancelled()) {
@@ -92,18 +82,17 @@ public class DyeableShulkers extends JavaPlugin implements Listener {
 
         shulker.setColor(dyeEvent.getColor() == null ? defaultColor : dyeEvent.getColor());
 
-        if (event.getPlayer().getGameMode() != CREATIVE) {
+        if (player.getGameMode() != GameMode.CREATIVE) {
             itemStack.setAmount(itemStack.getAmount() - 1);
         }
 
         if (server.isNewerThanOrEquals(1, 15, 2)) {
-            if (event.getPlayer().hasPermission("dyeableshulkers.dye.swing")) {
-                if (offhand) event.getPlayer().swingOffHand();
-                else event.getPlayer().swingMainHand();
+            if (player.hasPermission("dyeableshulkers.dye.swing")) {
+                if (offhand) player.swingOffHand(); else player.swingMainHand();
             }
 
-            if (server.isNewerThanOrEquals(1, 17, 0) && event.getPlayer().hasPermission("dyeableshulkers.dye.sound")) {
-                shulker.getWorld().playSound(shulker.getLocation(), ITEM_DYE_USE, PLAYERS, 1, 1);
+            if (server.isNewerThanOrEquals(1, 17, 0) && player.hasPermission("dyeableshulkers.dye.sound")) {
+                shulker.getWorld().playSound(shulker.getLocation(), Sound.ITEM_DYE_USE, SoundCategory.PLAYERS, 1, 1);
             }
         }
     }
@@ -114,7 +103,7 @@ public class DyeableShulkers extends JavaPlugin implements Listener {
         if (server.isOlderThan(1, 13, 0)) {
             if (itemStack.getType().getData() == Dye.class) {
                 final DyeColor color = ((Dye) itemStack.getData()).getColor();
-                return Optional.of(color == null ? BLACK : color);
+                return Optional.of(color == null ? DyeColor.BLACK : color);
             } else {
                 return null;
             }
@@ -122,30 +111,30 @@ public class DyeableShulkers extends JavaPlugin implements Listener {
 
         if (server.isOlderThan(1, 14, 0)) {
             switch (itemStack.getType()) {
-                case BONE_MEAL: return Optional.of(WHITE);
-                case INK_SAC: return Optional.of(BLACK);
-                case COCOA_BEANS: return Optional.of(BROWN);
-                case LAPIS_LAZULI: return Optional.of(BLUE);
+                case BONE_MEAL: return Optional.of(DyeColor.WHITE);
+                case INK_SAC: return Optional.of(DyeColor.BLACK);
+                case COCOA_BEANS: return Optional.of(DyeColor.BROWN);
+                case LAPIS_LAZULI: return Optional.of(DyeColor.BLUE);
             }
         }
 
         switch (itemStack.getType()) {
-            case WHITE_DYE: return Optional.of(WHITE);
-            case LIGHT_GRAY_DYE: return Optional.of(LIGHT_GRAY);
-            case GRAY_DYE: return Optional.of(GRAY);
-            case BLACK_DYE: return Optional.of(BLACK);
-            case BROWN_DYE: return Optional.of(BROWN);
-            case RED_DYE: return Optional.of(RED);
-            case ORANGE_DYE: return Optional.of(ORANGE);
-            case YELLOW_DYE: return Optional.of(YELLOW);
-            case LIME_DYE: return Optional.of(LIME);
-            case GREEN_DYE: return Optional.of(GREEN);
-            case CYAN_DYE: return Optional.of(CYAN);
-            case LIGHT_BLUE_DYE: return Optional.of(LIGHT_BLUE);
-            case BLUE_DYE: return Optional.of(BLUE);
-            case PURPLE_DYE: return Optional.of(PURPLE);
-            case MAGENTA_DYE: return Optional.of(MAGENTA);
-            case PINK_DYE: return Optional.of(PINK);
+            case WHITE_DYE: return Optional.of(DyeColor.WHITE);
+            case LIGHT_GRAY_DYE: return Optional.of(DyeColor.LIGHT_GRAY);
+            case GRAY_DYE: return Optional.of(DyeColor.GRAY);
+            case BLACK_DYE: return Optional.of(DyeColor.BLACK);
+            case BROWN_DYE: return Optional.of(DyeColor.BROWN);
+            case RED_DYE: return Optional.of(DyeColor.RED);
+            case ORANGE_DYE: return Optional.of(DyeColor.ORANGE);
+            case YELLOW_DYE: return Optional.of(DyeColor.YELLOW);
+            case LIME_DYE: return Optional.of(DyeColor.LIME);
+            case GREEN_DYE: return Optional.of(DyeColor.GREEN);
+            case CYAN_DYE: return Optional.of(DyeColor.CYAN);
+            case LIGHT_BLUE_DYE: return Optional.of(DyeColor.LIGHT_BLUE);
+            case BLUE_DYE: return Optional.of(DyeColor.BLUE);
+            case PURPLE_DYE: return Optional.of(DyeColor.PURPLE);
+            case MAGENTA_DYE: return Optional.of(DyeColor.MAGENTA);
+            case PINK_DYE: return Optional.of(DyeColor.PINK);
             default: return null;
         }
     }
--- src/main/java/io/github/maninmyvan/dyeableshulkers/event/ShulkerDyeEvent.java
+++ src/main/java/io/github/maninmyvan/dyeableshulkers/event/ShulkerDyeEvent.java
@@ -6,31 +6,26 @@ import org.bukkit.DyeColor;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Shulker;
 import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
-import org.bukkit.event.entity.EntityEvent;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 @Getter
-public final class ShulkerDyeEvent extends EntityEvent implements Cancellable {
+@Setter
+public final class ShulkerDyeEvent extends Event implements Cancellable {
     private static final @NotNull HandlerList handlers = new HandlerList();
 
-    @Setter
     private @Nullable DyeColor color;
     private final @NotNull Player player;
+    private final Shulker shulker;
 
-    @Setter
     private boolean cancelled;
 
-    public ShulkerDyeEvent(@NotNull Shulker entity, @NotNull Player player, @Nullable DyeColor color) {
-        super(entity);
+    public ShulkerDyeEvent(@NotNull Shulker shulker, @NotNull Player player, @Nullable DyeColor color) {
+        this.shulker = shulker;
         this.color = color;
         this.player = player;
-    }
-
-    @Override
-    public @NotNull Shulker getEntity() {
-        return (Shulker) this.entity;
     }
 
     @Override
@@ -38,6 +33,7 @@ public final class ShulkerDyeEvent extends EntityEvent implements Cancellable {
         return handlers;
     }
 
+    @SuppressWarnings("unused")
     public static @NotNull HandlerList getHandlerList() {
         return handlers;
     }
